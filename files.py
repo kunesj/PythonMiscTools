bf23f339939e32a7142a19f4c87dba4c0d85fcc8
Jiří Kuneš <jirka642@gmail.com>
2016-09-06 21:47:28 +0200

    modified:   files.py

--- files.py
+++ files.py
@@ -2,6 +2,7 @@
 #coding: utf-8
 
 import os
+import ntpath
 
 def getFilenameExtension(fname):
     """ Returns extension of filename or None if the is none. """
@@ -30,3 +31,11 @@ def getPathExtensionless(path):
     fname_extensionless = getFilenameExtensionless(os.path.basename(path))
     common_path = os.path.join(os.path.dirname(path), fname_extensionless)
     return common_path
+
+def getUniversalBasename(path):
+    """
+    Works for both Windows (\) and Unix (/) paths
+    Warning: Will return only part of filename on Linux IF there is '\' in it
+    """
+    head, tail = ntpath.split(path)
+    return tail or ntpath.basename(head)
