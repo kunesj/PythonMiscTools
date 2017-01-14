da7749487a25d77001ddfc99df10fbf842f52bf7
Jiří Kuneš <jirka642@gmail.com>
2017-01-14 15:16:06 +0100

    new file:   fileprint.py

---
+++ fileprint.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+#coding: utf-8
+
+def fileprint(strdata, openfile=None, quiet=False, newline=True):
+    """
+    Usage:
+        with open('f.txt', 'w') as f:
+            fileprint('helloworld', f)
+    """
+    if newline:
+        strdata += "\n"
+    if not quiet:
+        print(strdata, end="")
+    if openfile is not None:
+        openfile.write(strdata)
