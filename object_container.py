2539fa5b764be16cf32818844bc6542c88247880
Jiří Kuneš <jirka642@gmail.com>
2018-12-22 21:11:22 +0100

    new file:   object_container.py

---
+++ object_container.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# encoding: utf-8
+
+
+class ObjectContainer:
+    """
+    Useful for having global reference to object/variable that might not be definable at import,
+    or might need to be replaced by different object/variable later.
+    """
+
+    def __init__(self):
+        self._wrapped_object = None
+
+    def replace_wrapped_container(self, obj):
+        """ Name of this function MUST NOT have any conflicts with function names of wrapped object """
+        self._wrapped_object = obj
+
+    def __str__(self):
+        return str(self._wrapped_object)
+
+    def __getattr__(self, attr):
+        if attr in self.__dict__:
+            return getattr(self, attr)
+        return getattr(self._wrapped_object, attr)
