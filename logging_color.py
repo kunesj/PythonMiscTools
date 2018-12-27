abd79c3a777959a8ce01a4d2bfeceae7f7f25d50
Jiří Kuneš <jirka642@gmail.com>
2018-12-27 13:12:20 +0100

    new file:   logging_color.py

---
+++ logging_color.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import logging
+import sys
+import os
+try:
+    import colorlog
+except ImportError:
+    pass
+
+
+def setup_logging():
+    root = logging.getLogger()
+    root.setLevel(logging.DEBUG)
+    format = '%(log_color)s%(levelname)s:%(name)s:%(lineno)d: %(message)s%(reset)s'
+    date_format = '%Y-%m-%d %H:%M:%S'
+    if 'colorlog' in sys.modules and os.isatty(2):
+        cformat = '%(log_color)s' + format
+        f = colorlog.ColoredFormatter(
+            cformat,
+            date_format,
+            log_colors={
+                'DEBUG': 'reset',
+                'INFO': 'reset',
+                'WARNING': 'bold_yellow',
+                'ERROR': 'bold_red',
+                'CRITICAL': 'bold_red',
+            }
+        )
+    else:
+        f = logging.Formatter(format, date_format)
+    ch = logging.StreamHandler()
+    ch.setFormatter(f)
+    root.addHandler(ch)
