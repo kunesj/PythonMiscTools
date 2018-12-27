6e55b7f5d1d4b6b2b3524237a10f1d13f468dd4b
Jiří Kuneš <jirka642@gmail.com>
2018-12-27 23:17:14 +0100

    fixed:   declare_class.py

--- declare_class.py
+++ declare_class.py
@@ -1,13 +1,22 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-clsregistry = dict()
-
 
 class Meta(type):
     """ Make new classes always inherit from newest subclass """
-    def __new__(cls, name, bases, dct):
-        if name != 'Base':
+
+    _clsregistry = {}
+
+    def __new__(cls, name, bases, dct, _declarative_base=False):
+        if _declarative_base:
+            # init of Base class
+            new_class = super().__new__(cls, name, bases, dct)
+
+        else:
+            # Can only inherit from one class  # TODO: inherit from multiple classes
+            if len(bases) > 1:
+                raise Exception('Can only inherit from one class. Creating new Base class might help?')
+
             # add class name variable if missing. Try to parse from base class, if that fails set it to 'name'.
             if '__declared_class_name__' not in dct:
                 class_name = name
@@ -17,21 +26,18 @@ class Meta(type):
                         break
                 dct['__declared_class_name__'] = class_name
 
-            # update base classes to newest child class
-            tmp = []
-            for class_var in bases:
-                if hasattr(class_var, '__declared_class_name__') and class_var.__declared_class_name__ in clsregistry:
-                    class_var = clsregistry[class_var.__declared_class_name__]
-                tmp.append(class_var)
-            bases = tuple(tmp)
-
             # build new class and save for next request
-            new_class = super().__new__(cls, name, bases, dct)
-            clsregistry[dct['__declared_class_name__']] = new_class
+            if dct['__declared_class_name__'] not in cls._clsregistry:
+                new_class = super().__new__(cls, name, bases, dct)
+                cls._clsregistry[dct['__declared_class_name__']] = new_class
 
-        else:
-            # init of Base class
-            new_class = super().__new__(cls, name, bases, dct)
+            # update attributes of old class
+            else:
+                new_class = cls._clsregistry[dct['__declared_class_name__']]
+                for attr_key in dct:
+                    if attr_key in ['__module__', '__qualname__']:
+                        continue
+                    setattr(new_class, attr_key, dct[attr_key])
 
         return new_class
 
@@ -40,11 +46,9 @@ def declarative_base(base=None):
     """
     Tries to do what sqlalchemy.ext.declarative.declarative_base does with classes.
     But without any SQL or database table stuff. Just changing how Python inherits a class.
+
+    >>> Base = declarative_base()
     """
     bases = (base, ) if base else ()
     dct = {}
-    return Meta('Base', bases, dct)
-
-
-Base = declarative_base()
-
+    return Meta('Base', bases, dct, _declarative_base=True)
