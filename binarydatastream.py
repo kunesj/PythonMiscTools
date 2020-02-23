c8a915a5e49b27ffd25bdfe00b3be3b40dfb12c9
Jiří Kuneš <jirka642@gmail.com>
2020-02-23 19:00:32 +0100

    [binarydatastream] updated to python 3.8

--- binarydatastream.py
+++ binarydatastream.py
@@ -3,6 +3,7 @@
 
 import struct
 import io
+from typing import Union, Iterable
 
 
 class BinaryDataStream(io.BytesIO):
@@ -13,11 +14,10 @@ class BinaryDataStream(io.BytesIO):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self.byte_order = '<'  # little-endian, default on X86/x86_64
+        self.string_encoding = 'ascii'
 
-        self.byte_order = "<"  # little-endian, default on X86/x86_64
-        self.string_encoding = "ascii"
-
-    def get_length(self):
+    def get_length(self) -> int:
         """ Returns length of stream """
         last_pos = self.tell()
         self.seek(0x0, io.SEEK_END)  # seek EOF
@@ -25,7 +25,7 @@ class BinaryDataStream(io.BytesIO):
         self.seek(last_pos, io.SEEK_SET)
         return length
 
-    def set_byte_order(self, byte_order):
+    def set_byte_order(self, byte_order: str) -> None:
         """
         Set byte order of binary data in stream.
 
@@ -36,14 +36,14 @@ class BinaryDataStream(io.BytesIO):
         >           big-endian              standard    none
         !           network (= big-endian)  standard    none
         """
-        if byte_order in ["@", "=", "<", ">", "!"]:
+        if byte_order in ['@', '=', '<', '>', '!']:
             self.byte_order = byte_order
         else:
-            raise Exception("Unknown byte order value! (%s)" % byte_order)
+            raise Exception(f'Unknown byte order value! ({byte_order})')
 
     # Generic
 
-    def read_byte_array(self, length):
+    def read_byte_array(self, length: int) -> bytearray:
         """
         Returns bytearray() with read bytes.
         Input:
@@ -53,57 +53,57 @@ class BinaryDataStream(io.BytesIO):
         self.readinto(buff)
         return buff
 
-    def read_struct(self, length, struct_format):
+    def read_struct(self, length: int, struct_format: str) -> Iterable:
         """
         Works the same way as struct.unpack(fmt, buffer) function from standard Python library.
         https://docs.python.org/3/library/struct.html#struct.unpack
         """
         buff = self.read_byte_array(length)
-        if struct_format[0] in ["@", "=", "<", ">", "!"]:
+        if struct_format[0] in ['@', '=', '<', '>', '!']:
             # format string already has byte order char
             return struct.unpack(struct_format, buff)
         else:
             return struct.unpack(self.byte_order+struct_format, buff)
 
-    def read_type(self, type_str):
+    def read_type(self, type_str: str) -> Union[int, bool, float, str]:
         val_type = type_str.lower().strip()
 
-        if val_type == "int8":
+        if val_type == 'int8':
             val = self.read_int8()
-        elif val_type == "uint8":
+        elif val_type == 'uint8':
             val = self.read_uint8()
-        elif val_type == "bool8":
+        elif val_type == 'bool8':
             val = self.read_bool8()
-        elif val_type == "int16":
+        elif val_type == 'int16':
             val = self.read_int16()
-        elif val_type == "uint16":
+        elif val_type == 'uint16':
             val = self.read_uint16()
-        elif val_type == "int32":
+        elif val_type == 'int32':
             val = self.read_int32()
-        elif val_type == "uint32":
+        elif val_type == 'uint32':
             val = self.read_uint32()
-        elif val_type == "long32":
+        elif val_type == 'long32':
             val = self.read_long32()
-        elif val_type == "ulong32":
+        elif val_type == 'ulong32':
             val = self.read_ulong32()
-        elif val_type == "long64":
+        elif val_type == 'long64':
             val = self.read_long64()
-        elif val_type == "ulong64":
+        elif val_type == 'ulong64':
             val = self.read_ulong64()
-        elif val_type == "float32":
+        elif val_type == 'float32':
             val = self.read_float32()
-        elif val_type == "double64":
+        elif val_type == 'double64':
             val = self.read_double64()
-        elif val_type == "nullstring":
+        elif val_type == 'nullstring':
             val = self.read_string_null()
         else:
-            raise Exception("Unsupported value type %s" % val_type)
+            raise Exception(f'Unsupported value type {val_type}')
 
         return val
 
     # Numbers
 
-    def read_bits8(self, num=1):
+    def read_bits8(self, num: int = 1) -> list:
         """
         Reads 8 bits as bools
         Returns list of all bits as boolean values.
@@ -111,18 +111,18 @@ class BinaryDataStream(io.BytesIO):
             [num - number of values to read. Must be num>=1. (Default==1) ]
         """
         if num < 1:
-            raise Exception("Number of values to read must be greater or equal to 1!")
+            raise Exception('Number of values to read must be greater or equal to 1!')
         buff = self.read_byte_array(1*num)
         unpacked = struct.unpack(self.byte_order+str(num)+'B', buff)
 
         # convert to bits
         bits = []
         for byte in unpacked:
-            bits.extend([bool(int(bit_str)) for bit_str in "{:08b}".format(byte)])
+            bits.extend([bool(int(bit_str)) for bit_str in '{:08b}'.format(byte)])
 
         return bits
 
-    def read_int8(self, num=1):
+    def read_int8(self, num: int = 1) -> Union[int, Iterable]:
         """
         Reads 1 byte as Int8
         Returns tupple if reading multiple values, else returns just value.
@@ -130,7 +130,7 @@ class BinaryDataStream(io.BytesIO):
             [num - number of values to read. Must be num>=1. (Default==1) ]
         """
         if num < 1:
-            raise Exception("Number of values to read must be greater or equal to 1!")
+            raise Exception('Number of values to read must be greater or equal to 1!')
         buff = self.read_byte_array(1*num)
         unpacked = struct.unpack(self.byte_order+str(num)+'b', buff)
         if num > 1:
@@ -138,7 +138,7 @@ class BinaryDataStream(io.BytesIO):
         else:
             return unpacked[0]
 
-    def read_uint8(self, num=1):
+    def read_uint8(self, num: int = 1) -> Union[int, Iterable]:
         """
         Reads 1 byte as unsigned UInt8
         Returns tupple if reading multiple values, else returns just value.
@@ -146,7 +146,7 @@ class BinaryDataStream(io.BytesIO):
             [num - number of values to read. Must be num>=1. (Default==1) ]
         """
         if num < 1:
-            raise Exception("Number of values to read must be greater or equal to 1!")
+            raise Exception('Number of values to read must be greater or equal to 1!')
         buff = self.read_byte_array(1*num)
         unpacked = struct.unpack(self.byte_order+str(num)+'B', buff)
         if num > 1:
@@ -154,7 +154,7 @@ class BinaryDataStream(io.BytesIO):
         else:
             return unpacked[0]
 
-    def read_bool8(self, num=1):
+    def read_bool8(self, num: int = 1) -> Union[bool, Iterable]:
         """
         Reads 1 byte as bool8
         Returns tupple if reading multiple values, else returns just value.
@@ -162,7 +162,7 @@ class BinaryDataStream(io.BytesIO):
             [num - number of values to read. Must be num>=1. (Default==1) ]
         """
         if num < 1:
-            raise Exception("Number of values to read must be greater or equal to 1!")
+            raise Exception('Number of values to read must be greater or equal to 1!')
         buff = self.read_byte_array(1*num)
         unpacked = [v != 0 for v in struct.unpack(self.byte_order+str(num)+'B', buff)]
         if num > 1:
@@ -170,7 +170,7 @@ class BinaryDataStream(io.BytesIO):
         else:
             return unpacked[0]
 
-    def read_int16(self, num=1):
+    def read_int16(self, num: int = 1) -> Union[int, Iterable]:
         """
         Reads 2 bytes as Int16 (SHORT, short)
         Returns tupple if reading multiple values, else returns just value.
@@ -178,7 +178,7 @@ class BinaryDataStream(io.BytesIO):
             [num - number of values to read. Must be num>=1. (Default==1) ]
         """
         if num < 1:
-            raise Exception("Number of values to read must be greater or equal to 1!")
+            raise Exception('Number of values to read must be greater or equal to 1!')
         buff = self.read_byte_array(2*num)
         unpacked = struct.unpack(self.byte_order+str(num)+'h', buff)
         if num > 1:
@@ -186,7 +186,7 @@ class BinaryDataStream(io.BytesIO):
         else:
             return unpacked[0]
 
-    def read_uint16(self, num=1):
+    def read_uint16(self, num: int = 1) -> Union[int, Iterable]:
         """
         Reads 2 bytes as unsigned Int16 (WORD, unsigned short)
         Returns tupple if reading multiple values, else returns just value.
@@ -194,7 +194,7 @@ class BinaryDataStream(io.BytesIO):
             [num - number of values to read. Must be num>=1. (Default==1) ]
         """
         if num < 1:
-            raise Exception("Number of values to read must be greater or equal to 1!")
+            raise Exception('Number of values to read must be greater or equal to 1!')
         buff = self.read_byte_array(2*num)
         unpacked = struct.unpack(self.byte_order+str(num)+'H', buff)
         if num > 1:
@@ -202,7 +202,7 @@ class BinaryDataStream(io.BytesIO):
         else:
             return unpacked[0]
 
-    def read_int32(self, num=1):
+    def read_int32(self, num: int = 1) -> Union[int, Iterable]:
         """
         Reads 4 bytes as Int32 (int)
         Returns tupple if reading multiple values, else returns just value.
@@ -210,7 +210,7 @@ class BinaryDataStream(io.BytesIO):
             [num - number of values to read. Must be num>=1. (Default==1) ]
         """
         if num < 1:
-            raise Exception("Number of values to read must be greater or equal to 1!")
+            raise Exception('Number of values to read must be greater or equal to 1!')
         buff = self.read_byte_array(4*num)
         unpacked = struct.unpack(self.byte_order+str(num)+'i', buff)
         if num > 1:
@@ -218,7 +218,7 @@ class BinaryDataStream(io.BytesIO):
         else:
             return unpacked[0]
 
-    def read_uint32(self, num=1):
+    def read_uint32(self, num: int = 1) -> Union[int, Iterable]:
         """
         Reads 4 bytes as unsigned Int32 (DWORD, int)
         Returns tupple if reading multiple values, else returns just value.
@@ -226,7 +226,7 @@ class BinaryDataStream(io.BytesIO):
             [num - number of values to read. Must be num>=1. (Default==1) ]
         """
         if num < 1:
-            raise Exception("Number of values to read must be greater or equal to 1!")
+            raise Exception('Number of values to read must be greater or equal to 1!')
         buff = self.read_byte_array(4*num)
         unpacked = struct.unpack(self.byte_order+str(num)+'I', buff)
         if num > 1:
@@ -234,7 +234,7 @@ class BinaryDataStream(io.BytesIO):
         else:
             return unpacked[0]
 
-    def read_long32(self, num=1):
+    def read_long32(self, num: int = 1) -> Union[int, Iterable]:
         """
         Reads 4 bytes as Long32 (long)
         Returns tupple if reading multiple values, else returns just value.
@@ -242,7 +242,7 @@ class BinaryDataStream(io.BytesIO):
             [num - number of values to read. Must be num>=1. (Default==1) ]
         """
         if num < 1:
-            raise Exception("Number of values to read must be greater or equal to 1!")
+            raise Exception('Number of values to read must be greater or equal to 1!')
         buff = self.read_byte_array(4*num)
         unpacked = struct.unpack(self.byte_order+str(num)+'l', buff)
         if num > 1:
@@ -250,7 +250,7 @@ class BinaryDataStream(io.BytesIO):
         else:
             return unpacked[0]
 
-    def read_ulong32(self, num=1):
+    def read_ulong32(self, num: int = 1) -> Union[int, Iterable]:
         """
         Reads 4 bytes as unsigned Long32 (unsigned long)
         Returns tupple if reading multiple values, else returns just value.
@@ -258,7 +258,7 @@ class BinaryDataStream(io.BytesIO):
             [num - number of values to read. Must be num>=1. (Default==1) ]
         """
         if num < 1:
-            raise Exception("Number of values to read must be greater or equal to 1!")
+            raise Exception('Number of values to read must be greater or equal to 1!')
         buff = self.read_byte_array(4*num)
         unpacked = struct.unpack(self.byte_order+str(num)+'L', buff)
         if num > 1:
@@ -266,7 +266,7 @@ class BinaryDataStream(io.BytesIO):
         else:
             return unpacked[0]
 
-    def read_long64(self, num=1):
+    def read_long64(self, num: int = 1) -> Union[int, Iterable]:
         """
         Reads 8 bytes as unsigned Long64 (long long)
         Returns tupple if reading multiple values, else returns just value.
@@ -274,7 +274,7 @@ class BinaryDataStream(io.BytesIO):
             [num - number of values to read. Must be num>=1. (Default==1) ]
         """
         if num < 1:
-            raise Exception("Number of values to read must be greater or equal to 1!")
+            raise Exception('Number of values to read must be greater or equal to 1!')
         buff = self.read_byte_array(8*num)
         unpacked = struct.unpack(self.byte_order+str(num)+'q', buff)
         if num > 1:
@@ -282,7 +282,7 @@ class BinaryDataStream(io.BytesIO):
         else:
             return unpacked[0]
 
-    def read_ulong64(self, num=1):
+    def read_ulong64(self, num: int = 1) -> Union[int, Iterable]:
         """
         Reads 8 bytes as unsigned Long64 (unsigned long long)
         Returns tupple if reading multiple values, else returns just value.
@@ -290,7 +290,7 @@ class BinaryDataStream(io.BytesIO):
             [num - number of values to read. Must be num>=1. (Default==1) ]
         """
         if num < 1:
-            raise Exception("Number of values to read must be greater or equal to 1!")
+            raise Exception('Number of values to read must be greater or equal to 1!')
         buff = self.read_byte_array(8*num)
         unpacked = struct.unpack(self.byte_order+str(num)+'Q', buff)
         if num > 1:
@@ -298,7 +298,7 @@ class BinaryDataStream(io.BytesIO):
         else:
             return unpacked[0]
 
-    def read_float32(self, num=1):
+    def read_float32(self, num: int = 1) -> Union[float, Iterable]:
         """
         Reads 4 bytes as Float32 (float)
         Returns tupple if reading multiple values, else returns just value.
@@ -306,7 +306,7 @@ class BinaryDataStream(io.BytesIO):
             [num - number of values to read. Must be num>=1. (Default==1) ]
         """
         if num < 1:
-            raise Exception("Number of values to read must be greater or equal to 1!")
+            raise Exception('Number of values to read must be greater or equal to 1!')
         buff = self.read_byte_array(4*num)
         unpacked = struct.unpack(self.byte_order+str(num)+'f', buff)
         if num > 1:
@@ -314,7 +314,7 @@ class BinaryDataStream(io.BytesIO):
         else:
             return unpacked[0]
 
-    def read_double64(self, num=1):
+    def read_double64(self, num: int = 1) -> Union[float, Iterable]:
         """
         Reads 8 bytes as Double64 (double)
         Returns tupple if reading multiple values, else returns just value.
@@ -322,7 +322,7 @@ class BinaryDataStream(io.BytesIO):
             [num - number of values to read. Must be num>=1. (Default==1) ]
         """
         if num < 1:
-            raise Exception("Number of values to read must be greater or equal to 1!")
+            raise Exception('Number of values to read must be greater or equal to 1!')
         buff = self.read_byte_array(8*num)
         unpacked = struct.unpack(self.byte_order+str(num)+'d', buff)
         if num > 1:
@@ -332,13 +332,13 @@ class BinaryDataStream(io.BytesIO):
 
     # Strings
 
-    def set_string_encoding(self, encoding):
+    def set_string_encoding(self, encoding: Union[str, None]) -> None:
         """
         Enables to auto-decode read strings to unicode() with readNullString() function.
         Will return binary string if encoding is set to None.
         Input:
             Encoding of strings in binary data OR 'None' to disable automatic decoding.
-            (Examples: "utf-8", "ascii", "None", None)
+            (Examples: 'utf-8', 'ascii', 'None', None)
         """
         if not isinstance(encoding, str):
             self.string_encoding = None
@@ -347,7 +347,7 @@ class BinaryDataStream(io.BytesIO):
         else:
             self.string_encoding = encoding.lower()
 
-    def read_string(self, length, strip_null=False, raw=False):
+    def read_string(self, length: int, strip_null: bool = False, raw: bool = False) -> Union[str, bytes]:
         """
         Reads string of specified length from binary data.
 
@@ -366,14 +366,14 @@ class BinaryDataStream(io.BytesIO):
 
         return s
 
-    def read_string_null(self, raw=False):
+    def read_string_null(self, raw: bool = False) -> Union[str, bytes]:
         """
         Reads a null-terminated string of unspecified length from stream.
 
         Input:
             [raw - True if you want to disable auto-decoding for this function call]
         """
-        output_string = b""
+        output_string = b''
         char = self.read(1)
         while char != b'\x00':
             output_string += char
@@ -386,7 +386,7 @@ class BinaryDataStream(io.BytesIO):
 
         return output_string
 
-    def read_text(self, size=-1, raw=False):
+    def read_text(self, size: int = -1, raw: bool = False) -> Union[str, bytes]:
         data = super().read(size)
         if (self.string_encoding is not None) and (not raw):
             data = data.decode(self.string_encoding)
